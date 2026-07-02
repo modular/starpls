ca22c3316efcb86d401156a4cbfed1f1de08ca79
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2026-07-02 18:32:54 -0400

    ci: Misc fixes/modernization (#1)
    * ci: Misc fixes/modernization
    
    - We don't have a Buildbuddy key for this repo
    - Update actions and pin to SHAs
    - Remove old Mac images

--- bazel/setup-ci.sh
+++ bazel/setup-ci.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-echo 'import %workspace%/bazel/remote-cache.bazelrc' >>.bazelrc
-echo "build --remote_header=x-buildbuddy-api-key=$BUILDBUDDY_API_KEY" >>.bazelrc
+if [[ -n "$BUILDBUDDY_API_KEY" ]]; then
+  echo 'import %workspace%/bazel/remote-cache.bazelrc' >>.bazelrc
+  echo "build --remote_header=x-buildbuddy-api-key=$BUILDBUDDY_API_KEY" >>.bazelrc
+fi
